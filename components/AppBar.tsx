79c03c5ec164eb664da5ad4918fc2cff4bf80a5e
Deekshant Yadav <d33kshant@gmail.com>
2022-08-08 23:35:04 +0530

    Created appbar component

---
+++ components/AppBar.tsx
@@ -0,0 +1,68 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+import { IconButton, Toolbar, Typography, Stack, Avatar, Button, AppBar as AppBarMUI, MenuItem, Menu } from "@mui/material"
+import { Menu as MenuIcon } from "@mui/icons-material"
+import { useState } from "react"
+
+export default function ({ onMenuClick }) {
+	const { data, status } = useSession()
+	const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null)
+
+	const handleClose = () => {
+		setMenuAnchor(null)
+	}
+
+	return (
+		<AppBarMUI position="sticky" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+			<Stack direction="row" justifyContent="space-between">
+				<Toolbar variant="dense">
+					<Stack direction="row" alignItems="center" sx={{ sm: { px: 1 } }}>
+						{onMenuClick && (
+							<IconButton onClick={() => onMenuClick()} edge="start" color="inherit" aria-label="menu" sx={{ mr: 1 }}>
+								<MenuIcon />
+							</IconButton>
+						)}
+						<Typography variant="h6">HackSafe</Typography>
+					</Stack>
+				</Toolbar>
+				{status !== "loading" && (
+					<Stack direction="row" alignItems="center" px={1}>
+						{data?.user ? (
+							<IconButton onClick={(event) => setMenuAnchor(event.currentTarget)}>
+								<Avatar sx={{ height: "28px", width: "28px" }} alt="Profile" src={data?.user?.image} />
+								<Menu
+									id="demo-positioned-menu"
+									anchorEl={menuAnchor}
+									open={Boolean(menuAnchor)}
+									onClose={handleClose}
+									anchorOrigin={{
+										vertical: "top",
+										horizontal: "left",
+									}}
+									transformOrigin={{
+										vertical: "top",
+										horizontal: "left",
+									}}
+								>
+									<MenuItem onClick={handleClose}>Profile</MenuItem>
+									<MenuItem onClick={handleClose}>My account</MenuItem>
+									<MenuItem
+										onClick={() => {
+											signOut()
+											handleClose()
+										}}
+									>
+										Logout
+									</MenuItem>
+								</Menu>
+							</IconButton>
+						) : (
+							<Button onClick={() => signIn()} color="inherit">
+								Sign In
+							</Button>
+						)}
+					</Stack>
+				)}
+			</Stack>
+		</AppBarMUI>
+	)
+}
