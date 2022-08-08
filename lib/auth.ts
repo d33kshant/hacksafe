31b21ca6398a5baca61737ed27edc56144c36b30
Deekshant Yadav <d33kshant@gmail.com>
2022-08-08 23:39:03 +0530

    Updated function to return user from session

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,17 +3,11 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { unstable_getServerSession } from "next-auth"
 import { config } from "pages/api/auth/[...nextauth]"
 
-export async function authorize(req: NextApiRequest, res: NextApiResponse) {
+export async function getUserFromSession(req: NextApiRequest, res: NextApiResponse) {
 	const session = await unstable_getServerSession(req, res, config)
-	const user = session?.user
-	if (user) {
-		const email = user?.email
-		if (email) {
-			const _user = await prisma.user.findUnique({
-				where: { email },
-				select: { email: true, id: true, name: true },
-			})
-			if (_user) req["user"] = _user
-		}
+	if (session?.user?.email) {
+		const user = await prisma.user.findUnique({ where: { email: session.user.email } })
+		return user
 	}
+	return null
 }
