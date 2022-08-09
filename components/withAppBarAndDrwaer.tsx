8763df63c7e725334481b4ead13f3d1436786cfb
Deekshant Yadav <d33kshant@gmail.com>
2022-08-09 21:04:42 +0530

    Fixed drawer open behaviour issue

--- components/withAppBarAndDrwaer.tsx
+++ components/withAppBarAndDrwaer.tsx
@@ -1,15 +1,23 @@
-import { Stack } from "@mui/material"
+import { Stack, useMediaQuery } from "@mui/material"
 import { useState } from "react"
 import AppBar from "./AppBar"
 import SideBar from "./SideBar"
 
 export default function (page) {
 	const [drawerOpen, setDrawerOepn] = useState(false)
+	const smallScreen = useMediaQuery("(max-width: 750px)")
+
+	const openDrawer = () => {
+		if (smallScreen) {
+			setDrawerOepn(prev => !prev)
+		}
+	}
+
 	return (
 		<>
-			<AppBar onMenuClick={() => setDrawerOepn(false)} />
+			<AppBar onMenuClick={openDrawer} />
 			<Stack direction="row">
-				<SideBar open={drawerOpen} onClose={() => setDrawerOepn(false)} />
+				<SideBar variant={smallScreen ? "temporary" : "permanent"} open={drawerOpen} onClose={() => setDrawerOepn(false)} />
 				{page}
 			</Stack>
 		</>
