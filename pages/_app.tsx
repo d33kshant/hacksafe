cf6f7ab7a983d4fe4d6f148f95e04dec2fbe61f0
Deekshant Yadav <d33kshant@gmail.com>
2022-08-09 21:03:19 +0530

    Added style caching for mui and global viewport meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
-import { SessionProvider } from 'next-auth/react'
-import '../styles/globals.css'
+import Head from "next/head"
+import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from "@mui/material/styles"
+import { CacheProvider } from "@emotion/react"
+import CssBaseline from "@mui/material/CssBaseline"
+import theme from "@/lib/theme"
+import createEmotionCache from "@/lib/createEmotionCache"
+import "../styles/globals.css"
 
-function App({ Component, session, pageProps }) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+export default function MyApp(props) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps, session } = props
+	const getLayout = Component.getLayout || ((page) => page)
+
 	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<SessionProvider session={session}>{getLayout(<Component {...pageProps} />)}</SessionProvider>
+			</ThemeProvider>
+		</CacheProvider>
 	)
 }
-
-export default App
