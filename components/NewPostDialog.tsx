0c7ab25fbc13c732c4ac9e7a2b36b1667b01d280
Deekshant Yadav <d33kshant@gmail.com>
2022-08-11 00:03:18 +0530

    Created modal component for new post

---
+++ components/NewPostDialog.tsx
@@ -0,0 +1,48 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack, TextField, useMediaQuery, useTheme } from "@mui/material"
+import { useState } from "react"
+
+const INIT_STATE = {
+	title: "",
+	body: "",
+	tags: "",
+}
+
+export default function ({ open, onClose, onSubmit }) {
+	const theme = useTheme()
+	const fullScreem = useMediaQuery(theme.breakpoints.down("md"))
+
+	const [formState, setFormState] = useState(INIT_STATE)
+
+	const onInputChange = (event) => {
+		const _formState = { ...formState }
+		_formState[event.target.name] = event.target.value
+		setFormState(_formState)
+	}
+
+	return (
+		<Dialog fullScreen={fullScreem} open={open} onClose={onClose}>
+			<DialogTitle>Ask a question</DialogTitle>
+			<DialogContent>
+				<Stack gap={1}>
+					<DialogContentText>Make sure to describe your question in way that people can uderstand it easly, It make your question get answered ASAP.</DialogContentText>
+					<TextField name="title" value={formState.title} onChange={onInputChange} variant="filled" label="Title" placeholder="A short descriptive title" />
+					<TextField name="body" value={formState.body} onChange={onInputChange} multiline maxRows={10} minRows={4} variant="filled" label="Description" placeholder="Describe your question" />
+					<TextField name="tags" value={formState.tags} onChange={onInputChange} variant="filled" label="Tags" placeholder="Tags help get answered fast. eg: first-question, learn, new-post etc." />
+				</Stack>
+			</DialogContent>
+			<DialogActions>
+				<Button
+					onClick={() => {
+						onSubmit(formState)
+						setFormState(INIT_STATE)
+					}}
+				>
+					Post Question
+				</Button>
+				<Button onClick={onClose} color="error">
+					Cancel
+				</Button>
+			</DialogActions>
+		</Dialog>
+	)
+}
