0f5dc85c43955bfa4eb5db6bc968bc284036ee7f
Deekshant Yadav <d33kshant@gmail.com>
2022-08-12 23:26:36 +0530

    Created api for blog controller

---
+++ pages/api/blogs.ts
@@ -0,0 +1,18 @@
+import { createBlog, deleteBlog, getBlogs, updateBlog } from "@/controllers/blogs"
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	switch (req.method) {
+		case "GET":
+			return getBlogs(req, res)
+		case "POST":
+			return createBlog(req, res)
+		case "PUT":
+			return updateBlog(req, res)
+		case "DELETE":
+			return deleteBlog(req, res)
+		default:
+			return res.status(405).json({ error: `Invalid method '${req.method}'` })
+	}
+}
+
