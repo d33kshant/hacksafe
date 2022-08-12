fe67eab177cfed775e1d0efc8331575e5d4e897a
Deekshant Yadav <d33kshant@gmail.com>
2022-08-12 23:26:15 +0530

    Temporarly removed header from response

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,6 +1,5 @@
 export default async function (args) {
 	const response = await fetch(args)
-	const _headers = response.headers
 	const data = await response.json()
-	return { ...data, _headers }
+	return data
 }
