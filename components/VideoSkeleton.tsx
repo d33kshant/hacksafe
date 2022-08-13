f70470e642a91d81c0b14f1fbd3293042685301a
Deekshant Yadav <d33kshant@gmail.com>
2022-08-13 23:48:31 +0530

    Updated the image skeleton height

--- components/VideoSkeleton.tsx
+++ components/VideoSkeleton.tsx
@@ -1,9 +1,9 @@
-import { Grid, Skeleton } from "@mui/material";
+import { Grid, Skeleton } from "@mui/material"
 
-export default function(){
+export default function () {
 	return (
 		<Grid item xs={12} sm={6} md={3}>
-			<Skeleton variant="rectangular" height={200} />
+			<Skeleton variant="rectangular" height={170} />
 			<Skeleton variant="rectangular" height={18} sx={{ my: 1 }} />
 			<Skeleton variant="rectangular" height={16} width="60%" />
 		</Grid>
