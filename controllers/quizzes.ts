1e7cf34aa5b0675f884dafed49aa6038276c5be7
Deekshant Yadav <d33kshant@gmail.com>
2022-08-14 23:32:29 +0530

    Created controller for quizzes

---
+++ controllers/quizzes.ts
@@ -0,0 +1,56 @@
+import prisma from '@/lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const ITEMS_PER_PAGE = 10
+
+export async function getQuiz(req: NextApiRequest, res: NextApiResponse) {
+
+	const _page = Array.isArray(req.query.page) ? req.query.page[0] : req.query.page
+	const id = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id
+	const page = parseInt(_page) || 1
+	const offset = (page - 1) * ITEMS_PER_PAGE
+
+	try {
+		if (id) {
+			const _quiz = await prisma.quiz.findUnique({
+				where: { id },
+				include: {
+					questions: {
+						select: {
+							multiple: true,
+							question: true,
+							options: true,
+						}
+					}
+				}
+			})
+			if (_quiz) {
+				const quiz = { ..._quiz, likes: _quiz.likes.length }
+				res.json(quiz)
+			} else {
+				res.status(404).json({ error: "Quiz not found." })
+			}
+		} else {
+			const _quizzes = await prisma.quiz.findMany({
+				skip: offset,
+				take: ITEMS_PER_PAGE,
+				include: {
+					_count: true
+				}
+			})
+			const quizzes = _quizzes.map(quiz => {
+				const questions = quiz._count.questions
+				const likes = quiz.likes.length
+				delete quiz.likes
+				delete quiz._count
+				return { ...quiz, likes, questions }
+			})
+			res.json(quizzes)
+		}
+	} catch (error) {
+		res.status(500).json({
+			error: "Something went wrong.",
+			payload: error,
+		})
+	}
+}
