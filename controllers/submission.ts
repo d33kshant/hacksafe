750eaf303b9736d1c5e75f78d7a82e70a30b8ab8
Deekshant Yadav <d33kshant@gmail.com>
2022-08-15 23:44:21 +0530

    Created controller for submission

---
+++ controllers/submission.ts
@@ -0,0 +1,65 @@
+import prisma from "@/lib/prisma"
+import { getUserFromSession } from "@/lib/auth"
+import { NextApiRequest, NextApiResponse } from "next"
+
+export async function getSubmission(req: NextApiRequest, res: NextApiResponse) {
+	const id: string = Array.isArray(req.query.video) ? req.query.post[0] : req.query.video
+	const user = await getUserFromSession(req, res)
+	if (!user) return res.status(400).json({ error: "Authentication required" })
+
+	try {
+		const submission = await prisma.quizSubmission.findUnique({
+			where: { id },
+			include: {
+				quiz: true
+			}
+		})
+
+		if (submission.userId !== user.id) return res.json({ error: "Not allowed to view this submission." })
+
+		res.json(submission)
+	} catch (error) {
+		res.status(500).json({
+			error: "Something went wrong.",
+			payload: error,
+		})
+	}
+}
+
+export async function createdSubmission(req: NextApiRequest, res: NextApiResponse) {
+	const id: string = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id
+	const user = await getUserFromSession(req, res)
+	if (!user) return res.status(400).json({ error: "Authentication required" })
+
+	const _submission = req.body
+
+	// Validate submission format - Mapping<QuestionId, SelectedAnswersIndex>
+	for (const question of Object.keys(_submission)) {
+		const answers = _submission[question]
+		if (typeof question !== 'string' || Array.isArray(answers)) {
+			return res.status(400).json({ error: "Invalid submission format." })
+		}
+	}
+
+	try {
+		const exist = ((await prisma.quiz.count({ where: { id } })) === 1)
+		if (!exist) return res.status(404).json({ error: "Quiz not found." })
+
+		const submission = await prisma.quizSubmission.create({
+			data: {
+				answers: JSON.stringify(_submission),
+				quizId: id,
+				userId: user.id
+			}
+		})
+		res.json({
+			message: "Answers submitted successfully.",
+			submission
+		})
+	} catch (error) {
+		res.status(500).json({
+			error: "Something went wrong.",
+			payload: error,
+		})
+	}
+}
