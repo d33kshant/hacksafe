97df0d2fb4fbfd68e0ac504a79b452793a43821e
Deekshant Yadav <d33kshant@gmail.com>
2022-08-17 19:29:00 +0530

    Formatted with prettier and removed console log

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,7 +1,14 @@
 import BlogCard from "@/components/BlogCard"
 import withAppBarAndDrwaer from "@/components/withAppBarAndDrwaer"
 import fetcher from "@/lib/fetcher"
-import { Button, Chip, CircularProgress, Paper, Stack, Typography } from "@mui/material"
+import {
+	Button,
+	Chip,
+	CircularProgress,
+	Paper,
+	Stack,
+	Typography,
+} from "@mui/material"
 import useSWR from "swr"
 
 export default function BlogsPage() {
@@ -9,15 +16,37 @@ export default function BlogsPage() {
 
 	return (
 		<>
-			<Stack sx={{ p: 2, gap: 2 }} width="100%" direction="column" alignItems="center">
+			<Stack
+				sx={{ p: 2, gap: 2 }}
+				width="100%"
+				direction="column"
+				alignItems="center"
+			>
 				<Paper sx={{ width: "100%", maxWidth: "1200px", p: 1 }}>
-					<Stack direction="row" alignItems="center" justifyContent="space-between">
+					<Stack
+						direction="row"
+						alignItems="center"
+						justifyContent="space-between"
+					>
 						<Stack direction="row" gap={1}>
 							{["Most Recent", "Most Liked"].map((cat, index) => (
-								<Chip key={index} color={index === 0 ? "secondary" : "default"} clickable label={cat} />
+								<Chip
+									key={index}
+									color={
+										index === 0 ? "secondary" : "default"
+									}
+									clickable
+									label={cat}
+								/>
 							))}
 						</Stack>
-						<Button variant="contained" color="success" sx={{ boxShadow: 0 }}>
+						<Button
+							LinkComponent="a"
+							href="/blogs/editor"
+							variant="contained"
+							color="success"
+							sx={{ boxShadow: 0 }}
+						>
 							Write a blog
 						</Button>
 					</Stack>
@@ -31,7 +60,6 @@ export default function BlogsPage() {
 }
 
 function getContent(error: any, data: any) {
-	console.log(error, data)
 	if (error || data?.error) {
 		return <Typography>Failed to get data</Typography>
 	}
