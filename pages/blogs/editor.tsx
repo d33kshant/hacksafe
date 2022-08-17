edaa34adcfb3b70ed39314e1c46765c76c626495
Deekshant Yadav <d33kshant@gmail.com>
2022-08-17 19:28:24 +0530

    Formated with prettier created validator

--- pages/blogs/editor.tsx
+++ pages/blogs/editor.tsx
@@ -1,5 +1,14 @@
 import withAppBarAndDrwaer from "@/components/withAppBarAndDrwaer"
-import { Button, Divider, Paper, Stack, Tab, Tabs, TextField, Typography } from "@mui/material"
+import {
+	Button,
+	Divider,
+	Paper,
+	Stack,
+	Tab,
+	Tabs,
+	TextField,
+	Typography,
+} from "@mui/material"
 import { useEffect, useState } from "react"
 import { marked } from "marked"
 import { useRouter } from "next/router"
@@ -12,7 +21,8 @@ const INITIAL_STATE = {
 	tags: "",
 }
 
-const DEFAULT_MARKDOWN = '<p align="center">Nothing to show, Try writing some stuffs.</p>'
+const DEFAULT_MARKDOWN =
+	'<p align="center">Nothing to show, Try writing some stuffs.</p>'
 
 export default function BlogEditor() {
 	const router = useRouter()
@@ -31,9 +41,17 @@ export default function BlogEditor() {
 			const response = await fetch("/api/blogs?id=" + id)
 			const data = await response.json()
 
-			if (data.error) return alert(data.error)
+			if (data.error) {
+				setIsFetching(false)
+				return alert(data.error)
+			}
 
-			setFormState({ title: data.title, tags: data.tags.join(", "), body: data.body, banner: data.banner })
+			setFormState({
+				title: data.title,
+				tags: data.tags.join(", "),
+				body: data.body,
+				banner: data.banner,
+			})
 			setIsFetching(true)
 		}
 		if (id) {
@@ -49,14 +67,24 @@ export default function BlogEditor() {
 		setFormState(_formState)
 	}
 
-	const onInputChage = (event) => updateFormState(event.target.name, event.target.value)
+	const onInputChage = (event) =>
+		updateFormState(event.target.name, event.target.value)
 
 	function getMarkdownText(text: string) {
-		var rawMarkup = /* DOMPurify.sanitize( */ marked.parse(text || DEFAULT_MARKDOWN) /* ) */
+		var rawMarkup = /* DOMPurify.sanitize( */ marked.parse(
+			text || DEFAULT_MARKDOWN
+		) /* ) */
 		return { __html: rawMarkup }
 	}
 
+	const isValidForm = (form) => {
+		return form.title && form.body
+	}
+
 	const postTheBlog = () => {
+		if (!isValidForm(formState)) {
+			return alert("Blog must contain a title and some content")
+		}
 		if (isNew) {
 			// TODO: post
 		} else {
@@ -66,7 +94,12 @@ export default function BlogEditor() {
 
 	return (
 		<>
-			<Stack sx={{ p: 2, gap: 2 }} width="100%" direction="column" alignItems="center">
+			<Stack
+				sx={{ p: 2, gap: 2 }}
+				width="100%"
+				direction="column"
+				alignItems="center"
+			>
 				<Paper sx={{ width: "100%", maxWidth: "1200px" }}>
 					<Stack alignItems="ce" direction="row" py={1} px={2}>
 						<Typography flex={1} fontWeight={500}>
@@ -75,30 +108,70 @@ export default function BlogEditor() {
 					</Stack>
 					<Divider />
 					<Stack p={2} gap={2}>
-						<TextField name="title" onChange={onInputChage} fullWidth label="Title" placeholder="An interasting title for your blog" variant="filled" />
-						<TextField name="banner" onChange={onInputChage} fullWidth label="Banner" placeholder="URL for the banner image" variant="filled" />
-						<TextField name="tags" onChange={onInputChage} fullWidth label="Tags" placeholder="Seprate tags with comma, eg: password-sefty, tutorial, tips-and-tricks etc." variant="filled" />
+						<TextField
+							name="title"
+							onChange={onInputChage}
+							fullWidth
+							label="Title"
+							placeholder="An interasting title for your blog"
+							variant="filled"
+						/>
+						<TextField
+							name="banner"
+							onChange={onInputChage}
+							fullWidth
+							label="Banner"
+							placeholder="URL for the banner image"
+							variant="filled"
+						/>
+						<TextField
+							name="tags"
+							onChange={onInputChage}
+							fullWidth
+							label="Tags"
+							placeholder="Seprate tags with comma, eg: password-sefty, tutorial, tips-and-tricks etc."
+							variant="filled"
+						/>
 					</Stack>
 				</Paper>
 				<Paper sx={{ width: "100%", maxWidth: "1200px" }}>
 					<Tabs value={currentTab} onChange={changeTab}>
-						<Tab label="Editor" />
+						<Tab label="Edit" />
 						<Tab label="Preview" />
 					</Tabs>
 					<Divider />
 					<TabPanel currentTab={currentTab} index={0}>
-						<TextField value={formState.body} name="body" onChange={onInputChage} multiline minRows={10} variant="filled" placeholder="Start writting your blog with markdown" />
+						<TextField
+							value={formState.body}
+							name="body"
+							onChange={onInputChage}
+							multiline
+							minRows={10}
+							variant="filled"
+							placeholder="Start writting your blog with markdown"
+						/>
 					</TabPanel>
 					<TabPanel currentTab={currentTab} index={1}>
-						<div dangerouslySetInnerHTML={getMarkdownText(formState.body)}></div>
+						<div
+							dangerouslySetInnerHTML={getMarkdownText(
+								formState.body
+							)}
+						></div>
 					</TabPanel>
 					<Divider />
 					<Stack p={2} justifyContent="end">
-						<Button onClick={postTheBlog} sx={{ boxShadow: 0 }} variant="contained" color="success">
-							Post the blog
+						<Button
+							onClick={postTheBlog}
+							sx={{ boxShadow: 0 }}
+							variant="contained"
+							color="success"
+							disabled={!isValidForm(formState)}
+						>
+							{isNew ? "Publish" : "Update"}
 						</Button>
 					</Stack>
 				</Paper>
+				<Divider />
 			</Stack>
 		</>
 	)
