532b0df4848f5e24ddc6334daf48d9e66cb5cd4e
Deekshant Yadav <d33kshant@gmail.com>
2022-08-18 22:08:02 +0530

    Added DOMPurify and formatted

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,6 +1,13 @@
 import withAppBarAndDrwaer from "@/components/withAppBarAndDrwaer"
-import { CircularProgress, Divider, Paper, Stack, Typography } from "@mui/material"
+import {
+	CircularProgress,
+	Divider,
+	Paper,
+	Stack,
+	Typography,
+} from "@mui/material"
 import { marked } from "marked"
+import DOMPurify from "isomorphic-dompurify"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
@@ -27,7 +34,12 @@ export default function BlogPage() {
 
 	return (
 		<>
-			<Stack sx={{ p: 2, gap: 2 }} width="100%" direction="column" alignItems="center">
+			<Stack
+				sx={{ p: 2, gap: 2 }}
+				width="100%"
+				direction="column"
+				alignItems="center"
+			>
 				<Paper sx={{ width: "100%", maxWidth: "1200px" }}>
 					{loading && (
 						<Stack alignItems="center" pt={2}>
@@ -38,14 +50,31 @@ export default function BlogPage() {
 					{!loading && blog?.title && (
 						<>
 							<Stack p={2}>
-								<img style={{ width: "100%", height: 250, objectFit: "cover", borderRadius: 8 }} src={blog.banner} />
+								<img
+									style={{
+										width: "100%",
+										height: 250,
+										objectFit: "cover",
+										borderRadius: 8,
+									}}
+									src={blog.banner}
+								/>
 								<Typography mt={2} variant="h4">
 									{blog.title}
 								</Typography>
 							</Stack>
 							<Divider />
 							<Stack p={2}>
-								<div dangerouslySetInnerHTML={{ __html: marked.parse(blog.body || "Failed to render this blog") }}></div>
+								<div
+									dangerouslySetInnerHTML={{
+										__html: DOMPurify.sanitize(
+											marked.parse(
+												blog.body ||
+													"Failed to render this blog"
+											)
+										),
+									}}
+								></div>
 							</Stack>
 						</>
 					)}
