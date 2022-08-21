7016d29a4f71dd2f4c6da1315a89ddf48c75904a
Deekshant Yadav <d33kshant@gmail.com>
2022-08-21 21:07:03 +0530

    Formatted structure

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -5,7 +5,16 @@ export default function ({ data: { id, title, thumbnail, views, updatedAt } }) {
 	return (
 		<Grid item xs={12} sm={6} md={3} component="a" href={`/videos/${id}`}>
 			<Stack direction="column">
-				<img style={{ width: "100%", objectFit: "cover", marginBottom: 4, borderRadius: 4 }} src={thumbnail} />
+				<img
+					style={{
+						width: "100%",
+						maxWidth: "250px",
+						objectFit: "cover",
+						marginBottom: 4,
+						borderRadius: 4,
+					}}
+					src={thumbnail}
+				/>
 				<Typography variant="body1">{title}</Typography>
 				<Typography color="gray" variant="body2">
 					{views} Views • {moment(updatedAt).fromNow()}
